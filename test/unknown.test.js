96c81934bcb6426e429cc50e0c995e6a3910eb14
dword <sebastianlandw@gmail.com>
2020-02-06 16:13:51 +0000

    remove expect

--- test/unknown.test.js
+++ test/unknown.test.js
@@ -1,5 +1,4 @@
 import getProjectzReadmeSectionRegex from 'get-projectz-readme-section-regex'
-import expect from 'expect'
 import { endent } from '@dword-design/functions'
 
 export default () => {
