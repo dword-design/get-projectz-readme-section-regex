463e530ca989dc3996d849f75a4a776778a787d5
Sebastian Landwehr <sebastianlandw@gmail.com>
2020-05-27 22:40:42 +0200

    fix: browser compatibility

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ export default regexName =>
     [
       `^<!--\\s*${regexName}\\s*-->`,
       '|',
-      `^<!--\\s*${regexName}/\\s*-->\\s*(.*?)\\s*^<!--\\s*/${regexName}\\s*-->`,
+      `^<!--\\s*${regexName}/\\s*-->\\s*((.|\\n)*?)\\s*^<!--\\s*/${regexName}\\s*-->`,
     ] |> join(''),
-    'mis'
+    'mi'
   )
